774b73270804c4bd895c31387fc9be4d82a6a56a
DucAnh(fungda) <69286705+spaghetti-lover@users.noreply.github.com>
2023-11-20 16:46:48 +0700

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import animatefx.animation.Bounce;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/main.fxml"));
+            Parent root = loader.load();
+            MainController controller = loader.getController();
+            controller.setPrimaryStage(primaryStage);
+            primaryStage.setTitle("Flash cards");
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add("/resources/css/main.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
